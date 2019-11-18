82891575123a4253a564d4a247642090e48c6540
mdavis88 <54687892+mdavis88@users.noreply.github.com>
2019-11-17 20:35:48 -1000

    Add files via upload

---
+++ Assignment 1/Product.data.js
@@ -0,0 +1,29 @@
+// my complete product data that will be presented on my website
+products = [
+        {
+            "model": "Hayden Shapes FutureFlex",
+            "price": 700.00,
+            "image": "./images/surfboard1.jpg"
+        },
+        {
+            "model": "Hayden Shapes Holy Grail",
+            "price": 600.00,
+            "image": "./images/surfboard2.jpg"
+        },
+        {
+            "model": "Waikiki Red Hot",
+            "price": 900.00,
+            "image": "./images/surfboard3.jpg"
+        },
+        {
+            "model": "Mark Richards Retro",
+            "price": 560.00,
+            "image": "./images/surfboard4.jpg"
+        },
+        {
+            "model": "Lost Maysym",  
+            "price": 499.00,
+            "image": "./images/surfboard5.jpg"
+        } ];
+if (typeof module != 'undefined') {
+    module.exports.products = products;}
